2552fb4afa2026c588806b203af4fd070263f5bf
paleotommytechy <tomiwadesignandtech00@gmail.com>
2025-02-13 10:30:30 +0100

    Second commit

--- accf/settings.py
+++ accf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i#3o9z8i0st6s-b8%hjq@+$*md6bl#j7d2oajupv2n#z^ej%^v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS","accfIkoleWebsite.onrender.com").split(",")
 
 
 # Application definition
