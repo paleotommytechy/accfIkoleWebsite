0bb13202692b97b3cd30e6b6b970b3518ebe4237
Ifeoluwa <137769237+paleotommytechy@users.noreply.github.com>
2025-02-19 14:29:17 +0100

    Update urls.py
    Deleted privacy_view function

--- website/urls.py
+++ website/urls.py
@@ -3,8 +3,6 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-def privacy_view(request):
-    return render(request, 'privacy.html')
 
 urlpatterns = [
     path('login/',views.login_view, name='login'),
