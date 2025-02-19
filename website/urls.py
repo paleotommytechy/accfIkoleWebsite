8088d7759bf028cbed6552efbdd452c2d60e15dd
Ifeoluwa <137769237+paleotommytechy@users.noreply.github.com>
2025-02-19 14:23:33 +0100

    Update urls.py
    Added privacy url and view function

--- website/urls.py
+++ website/urls.py
@@ -3,11 +3,15 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+def privacy_view(request):
+    return render(request, 'privacy.html')
+
 urlpatterns = [
     path('login/',views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('', views.dashboard, name='dashboard'),
+    path('privacy/', privacy_view, name='privacy'),
 ]
 if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
